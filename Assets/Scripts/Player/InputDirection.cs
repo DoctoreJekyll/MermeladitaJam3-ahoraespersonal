bd8aaad2ded91faddf76963ba5f1e870679395cc
Joseajierro <46693012+DoctoreJekyll@users.noreply.github.com>
2024-07-12 00:45:31 +0200

    Jump stuff and metas

--- Assets/Scripts/Player/InputDirection.cs
+++ Assets/Scripts/Player/InputDirection.cs
@@ -1,10 +1,15 @@
+using System;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.InputSystem;
 
 public class InputDirection : MonoBehaviour
 {
 
     [SerializeField] private InputActionReference inputMove;
+    [SerializeField] private InputActionReference inputJump;
+    [SerializeField] private UnityEvent jumpEvent;
+    [SerializeField] private bool letJump = false;
     private Vector2 direction;
     
     public float InputXDirectionValue()
@@ -14,5 +19,35 @@ public class InputDirection : MonoBehaviour
         return xValue;
     }
 
+    public bool JumpInput()
+    {
+        return letJump;
+    }
 
+    private void OnEnable()
+    {
+        inputJump.action.performed += JumpAction;
+        inputJump.action.canceled += JumpAction;
+    }
+
+    private void OnDisable()
+    {
+        inputJump.action.performed -= JumpAction;
+        inputJump.action.canceled -= JumpAction;
+    }
+
+    private void JumpAction(InputAction.CallbackContext obj)
+    {
+        if (obj.performed)
+        {
+            jumpEvent.Invoke();
+            letJump = true;
+            Debug.Log("press");
+        }
+        else if (obj.canceled)
+        {
+            letJump = false;
+            Debug.Log("unpress");
+        }
+    }
 }
