82dcf8b22c51fc9bff1eae369da1d35236386a06
Joseajierro <46693012+DoctoreJekyll@users.noreply.github.com>
2024-07-12 23:46:48 +0200

    Wall grab 1 iteration

--- Assets/Scripts/Player/WallGrab.cs
+++ Assets/Scripts/Player/WallGrab.cs
@@ -1,18 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
+using Jugador.NewWaterPlayer;
 using UnityEngine;
 
 public class WallGrab : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("Configuration")]
+    [SerializeField] private bool onWall;
+    [SerializeField] private Vector2 rigthOffset;
+    [SerializeField] private Vector2 leftOffset;
+    [SerializeField] private float collisionRaidus;
+    [SerializeField] private LayerMask groundLayer;
 
+    [Header("Extra Components")] 
+    [SerializeField] private Rigidbody2D rb2d;
+    [SerializeField] private ImproveJump improveJump;
+    
     // Update is called once per frame
     void Update()
     {
+        onWall = Physics2D.OverlapCircle((Vector2)transform.position + rigthOffset, collisionRaidus, groundLayer)
+            || Physics2D.OverlapCircle((Vector2)transform.position + leftOffset, collisionRaidus, groundLayer);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
         
+        Gizmos.DrawWireSphere((Vector2)transform.position + rigthOffset, collisionRaidus);
+        Gizmos.DrawWireSphere((Vector2)transform.position + leftOffset, collisionRaidus);
     }
 }
