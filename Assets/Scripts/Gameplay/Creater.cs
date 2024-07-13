ed24f5b3cdb39f6d90a6815e3701b4754d49ffbc
Joseajierro <46693012+DoctoreJekyll@users.noreply.github.com>
2024-07-13 03:18:34 +0200

    First iteration zeus rayos

--- Assets/Scripts/Gameplay/Creater.cs
+++ Assets/Scripts/Gameplay/Creater.cs
@@ -1,17 +1,45 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using Unity.Mathematics;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Creater : MonoBehaviour
 {
     [SerializeField] private GameObject objToCreate;
+    [SerializeField] private float timeToCreate;
+    private float xPosLeft;
+    private float xPostRigth;
+    private float timeToSpawn;
 
-    private void InstantiateObject()
+    Camera mainCamera;
+    private Vector3 topLeft;
+    private Vector3 topRight;
+    void Start()
     {
-        GameObject obj = Instantiate(objToCreate, transform.position, quaternion.identity);
+        mainCamera = Camera.main; // Obtener la cámara principal
+                
+        topLeft = mainCamera.ViewportToWorldPoint(new Vector3(0, 1, mainCamera.nearClipPlane));
+        topRight = mainCamera.ViewportToWorldPoint(new Vector3(1, 1, mainCamera.nearClipPlane));
+
+        xPosLeft = topLeft.x;
+        xPostRigth = topRight.x;
     }
     
+    private void InstantiateObject()
+    {
+        Vector3 randomPos = new Vector3(Random.Range(xPosLeft, xPostRigth), topLeft.y + 1, 0);
+        GameObject obj = Instantiate(objToCreate, randomPos, quaternion.identity);
+    }
 
-
+    private void Update()
+    {
+        timeToSpawn += Time.deltaTime;
+        
+        if (timeToSpawn > timeToCreate)
+        {
+            InstantiateObject();
+            timeToSpawn = 0;
+            Debug.Log("Projectil");
+        }
+    }
 }
--- Assets/Scripts/Gameplay/Zeus/Projectil.cs
+++ Assets/Scripts/Gameplay/Zeus/Projectil.cs
@@ -1,18 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Projectil : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnCollisionEnter2D(Collision2D other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Rigidbody2D rigidbody2D = other.gameObject.GetComponent<Rigidbody2D>();
+            rigidbody2D.AddForce(new Vector2(1,1) * 1000);
+        }
+        Destroy(this.gameObject);
     }
 }
