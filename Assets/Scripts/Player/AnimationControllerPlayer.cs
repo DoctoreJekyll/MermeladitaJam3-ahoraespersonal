bf756b9f3ad446ce21ee00eb63973169636cde2e
Joseajierro <46693012+DoctoreJekyll@users.noreply.github.com>
2024-07-13 19:28:06 +0200

    start anims

--- Assets/Scripts/Player/AnimationControllerPlayer.cs
+++ Assets/Scripts/Player/AnimationControllerPlayer.cs
@@ -1,12 +1,15 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Jugador.NewWaterPlayer;
 using UnityEngine;
 
 public class AnimationControllerPlayer : MonoBehaviour
 {
 
     [SerializeField] private InputDirection direction;
+    [SerializeField] private Rigidbody2D rb2d;
+    [SerializeField] private PlayerJump playerJump;
     private Animator animator;
 
     private void Start()
@@ -16,15 +19,13 @@ public class AnimationControllerPlayer : MonoBehaviour
 
     private void Update()
     {
-        if (direction.Direction().x > 0)
+        if (direction.Direction().x > 0 || direction.Direction().x < 0)
         {
-            animator.SetBool("Idle", false);
-            animator.SetBool("Run", true);
+            animator.Play("PlayerRun");
         }
         else
         {
-            animator.SetBool("Idle", true);
-            animator.SetBool("Run", false);
+            animator.Play("PlayerIdle");
         }
     }
 }
