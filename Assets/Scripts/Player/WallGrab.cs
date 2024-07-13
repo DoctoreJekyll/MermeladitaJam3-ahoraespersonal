c3177ffbdb023b6c44ac5347444366165a6ef9d6
Joseajierro <46693012+DoctoreJekyll@users.noreply.github.com>
2024-07-13 15:54:06 +0200

    refactor/paper script

--- Assets/Scripts/Player/WallGrab.cs
+++ Assets/Scripts/Player/WallGrab.cs
@@ -17,6 +17,8 @@ public class WallGrab : MonoBehaviour
 
     [Header("Climb values")] 
     [SerializeField] private float slideSpeed;
+    [SerializeField] private float maxTimeClimb;
+    private float timeClimbing;
 
     [Header("Extra Components")] 
     [SerializeField] private Rigidbody2D rb2d;
@@ -25,6 +27,8 @@ public class WallGrab : MonoBehaviour
     [SerializeField] private InputDirection direction;
 
     private float actualGravityScale;
+    private Flip flip;
+    private PlayerJump playerJump;
     
     // Eventos personalizados
     public UnityEvent OnRightWallExit;
@@ -32,6 +36,8 @@ public class WallGrab : MonoBehaviour
 
     private void Start()
     {
+        playerJump = GetComponent<PlayerJump>();
+        flip = GetComponent<Flip>();
         actualGravityScale = rb2d.gravityScale;
         
         // Inicializar eventos
@@ -58,7 +64,6 @@ public class WallGrab : MonoBehaviour
 
     private void OnExitLeftWall()
     {
-        Debug.Log("Saliendo de la pared izquierda");
         // Aquí puedes agregar la lógica que deseas ejecutar
         rb2d.AddForce(Vector2.up * upForce);
         move.enabled = true;
@@ -73,13 +78,20 @@ public class WallGrab : MonoBehaviour
         // Detectar cambio de estado en isOnRightWall
         if (isOnRightWall && !currentRightWall)
         {
-            OnRightWallExit.Invoke();
+            if (grab)
+            {
+                OnRightWallExit.Invoke();
+            }
+            
         }
 
         // Detectar cambio de estado en isOnLeftWall
         if (isOnLeftWall && !currentLeftWall)
         {
-            OnLeftWallExit.Invoke();
+            if (grab)
+            {
+                OnLeftWallExit.Invoke();
+            }
         }
 
         // Actualizar estados
@@ -98,8 +110,12 @@ public class WallGrab : MonoBehaviour
         }
         
         Grab();
-
-
+        
+        if (playerJump.isOnFloor)
+        {
+            Debug.Log("reset grab");
+            StopCoroutine(test);
+        }
 
     }
 
@@ -109,17 +125,35 @@ public class WallGrab : MonoBehaviour
         if (onWall && grab)
         {
             rb2d.velocity = new Vector2(rb2d.velocity.x, direction.InputYDirectionValue() * speed);
+            flip.enabled = false;
+        }
+        else
+        {
+            flip.enabled = true;
         }
     }
 
     private bool grab;
+    private Coroutine test;
     public void GrabInputPut()
     {
+        FallFromGrab();
         grab = true;
+    }
+
+    private void FallFromGrab()
+    {
+        test = StartCoroutine(TimeCorroutine());
     }
 
     public void GranInputRelease()
     {
+        grab = false;
+    }
+
+    IEnumerator TimeCorroutine()
+    {
+        yield return new WaitForSeconds(3f);
         grab = false;
     }
 
---
+++ Assets/Scripts/Props/Paper.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paper : MonoBehaviour
+{
+    
+    
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        throw new NotImplementedException();
+    }
+}
